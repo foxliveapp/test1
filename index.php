cd27191cf20632bfa90f1e44f3ed22401b17c281
foxliveapp <136158949+foxliveapp@users.noreply.github.com>
2023-06-12 08:12:11 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Foxapp</title>
+        <link rel="icon" type="image/png" sizes="32x32" href="img/favicon.png">
+       <meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="theme-color" content="#2a41e8">
+		<link rel="icon" sizes="192x192" href="img/favicon.png">
+		<meta name="msapplication-navbutton-color" content="#2a41e8">
+		<meta name="apple-mobile-web-app-status-bar-style" content="#2a41e8"> 
+        <!-- favicon -->
+        <link rel="shortcut icon" href="img/favicon.png">
+        <link rel="apple-touch-icon" href="img/favicon.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="img/favicon.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="img/favicon.png">
+        
+  <style>
+    body {
+        background: #771d41 }
+    section {
+        padding: 1em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%) }
+  </style>
+  <script src="OneSignalSDKWorker.js"></script>
+    <script src="OneSignalMethods.js"></script>
+  </head>
+  <body>
+  <section>
+    <h1 style="color:#fff;font-size:40px">Foxapp</h1>
+  </section>
+  
+ 
+</body>
+
+
+</html>
